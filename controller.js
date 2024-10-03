95a62a51cd01f5e2496fad42f432fcd4f6230938
Lamz16 <andiku0755@gmail.com>
2024-10-03 13:13:57 +0700

    update categorry,disscussions,news,regencies,users get all data and get data by id

--- controller.js
+++ controller.js
@@ -30,4 +30,161 @@ exports.tampilComplaintId = function (req, res) {
       }
     );
 };
+
+exports.getAllCategories = function (req, res) {
+    connection.query(
+        "SELECT * FROM categories",
+        function (error, rows) {
+            if (error) {
+                console.log(error);  // Log error untuk debugging
+                res.status(500).send("Error retrieving data");
+            } else {
+                res.status(200).json(rows);  // Kirim data dalam format JSON
+            }
+        }
+    );
+};
+
+exports.getCategoryById = function (req, res) {
+    let id = req.params.id;
+    console.log("ID yang diterima:", id);  // Tambahkan log ini untuk debugging
+    connection.query(
+        "SELECT * FROM categories WHERE id = ?", [id],
+        function (error, rows) {
+            if (error) {
+                console.log(error);  // Log error untuk debugging
+                res.status(500).send("Error retrieving data");
+            } else {
+                res.status(200).json(rows);  // Kirim data dalam format JSON
+            }
+        }
+    );
+};
+
+
+exports.getAllDiscussions = function (req, res) {
+    connection.query(
+        "SELECT * FROM discussions",
+        function (error, rows) {
+            if (error) {
+                console.log(error);  // Log error untuk debugging
+                res.status(500).send("Error retrieving data");
+            } else {
+                res.status(200).json(rows);  // Kirim data dalam format JSON
+            }
+        }
+    );
+};
+
+exports.getDiscussionById = function (req, res) {
+    let id = req.params.id;
+    console.log("ID yang diterima:", id);  // Tambahkan log ini untuk debugging
+    connection.query(
+        "SELECT * FROM discussions WHERE id = ?", [id],
+        function (error, rows) {
+            if (error) {
+                console.log(error);  // Log error untuk debugging
+                res.status(500).send("Error retrieving data");
+            } else {
+                res.status(200).json(rows);  // Kirim data dalam format JSON
+            }
+        }
+    );
+};
+
+
+exports.getAllNews = function (req, res) {
+    connection.query(
+        "SELECT * FROM news",
+        function (error, rows) {
+            if (error) {
+                console.log(error);  // Log error untuk debugging
+                res.status(500).send("Error retrieving data");
+            } else {
+                res.status(200).json(rows);  // Kirim data dalam format JSON
+            }
+        }
+    );
+};
+
+
+exports.getNewsById = function (req, res) {
+    let id = req.params.id;
+    console.log("ID yang diterima:", id);  // Tambahkan log ini untuk debugging
+    connection.query(
+        "SELECT * FROM news WHERE id = ?", [id],
+        function (error, rows) {
+            if (error) {
+                console.log(error);  // Log error untuk debugging
+                res.status(500).send("Error retrieving data");
+            } else {
+                res.status(200).json(rows);  // Kirim data dalam format JSON
+            }
+        }
+    );
+};
+
+exports.getAllRegencies = function (req, res) {
+    connection.query(
+        "SELECT * FROM regencies",
+        function (error, rows) {
+            if (error) {
+                console.log(error);  // Log error untuk debugging
+                res.status(500).send("Error retrieving data");
+            } else {
+                res.status(200).json(rows);  // Kirim data dalam format JSON
+            }
+        }
+    );
+};
+
+
+exports.getRegencyById = function (req, res) {
+    let id = req.params.id;
+    console.log("ID yang diterima:", id);  // Tambahkan log ini untuk debugging
+    connection.query(
+        "SELECT * FROM regencies WHERE id = ?", [id],
+        function (error, rows) {
+            if (error) {
+                console.log(error);  // Log error untuk debugging
+                res.status(500).send("Error retrieving data");
+            } else {
+                res.status(200).json(rows);  // Kirim data dalam format JSON
+            }
+        }
+    );
+};
+
+
+exports.getAllUsers = function (req, res) {
+    connection.query(
+        "SELECT * FROM users",
+        function (error, rows) {
+            if (error) {
+                console.log(error);  // Log error untuk debugging
+                res.status(500).send("Error retrieving data");
+            } else {
+                res.status(200).json(rows);  // Kirim data dalam format JSON
+            }
+        }
+    );
+};
+
+
+exports.getUserById = function (req, res) {
+    let id = req.params.id;
+    console.log("ID yang diterima:", id);  // Tambahkan log ini untuk debugging
+    connection.query(
+        "SELECT * FROM users WHERE id = ?", [id],
+        function (error, rows) {
+            if (error) {
+                console.log(error);  // Log error untuk debugging
+                res.status(500).send("Error retrieving data");
+            } else {
+                res.status(200).json(rows);  // Kirim data dalam format JSON
+            }
+        }
+    );
+};
+
   
--- routes.js
+++ routes.js
@@ -1,15 +1,57 @@
 "use strict";
 
 module.exports = function (app) {
-  var jsonku = require("./controller");
+    var jsonku = require("./controller");
 
-  app.route("/")
-  .get(jsonku.index);
+    app.route("/")
+        .get(jsonku.index);
 
-  app.route("/complaints")
-  .get(jsonku.tampilComplaints);
+    app.route("/complaints")
+        .get(jsonku.tampilComplaints);
 
-  app.route("/complaint-detail/:id")
-  .get(jsonku.tampilComplaintId);
+    /*get by id detail complaint*/
+    app.route("/complaint-detail/:id")
+        .get(jsonku.tampilComplaintId);
+
+    // Mengambil semua kategori
+    app.route("/categories")
+        .get(jsonku.getAllCategories);
+
+// Mengambil kategori berdasarkan ID
+    app.route("/categories/:id")
+        .get(jsonku.getCategoryById);
+
+
+    // Mengambil semua diskusi
+    app.route("/discussions")
+        .get(jsonku.getAllDiscussions);
+
+// Mengambil diskusi berdasarkan ID
+    app.route("/discussions/:id")
+        .get(jsonku.getDiscussionById);
+
+// Mengambil semua berita
+    app.route("/news")
+        .get(jsonku.getAllNews);
+
+// Mengambil berita berdasarkan ID
+    app.route("/news/:id")
+        .get(jsonku.getNewsById);
+
+// Mengambil semua daerah (regencies)
+    app.route("/regencies")
+        .get(jsonku.getAllRegencies);
+
+// Mengambil daerah berdasarkan ID
+    app.route("/regencies/:id")
+        .get(jsonku.getRegencyById);
+
+    // Mengambil semua pengguna
+    app.route("/users")
+        .get(jsonku.getAllUsers);
+
+// Mengambil pengguna berdasarkan ID
+    app.route("/users/:id")
+        .get(jsonku.getUserById);
 
 };
